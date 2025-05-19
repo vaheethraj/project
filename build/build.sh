798d1ac55ffe58b9340b365a59823c33ff6d94ee
vaheethraj <128178219+vaheethraj@users.noreply.github.com>
2025-05-19 10:13:22 +0530

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -7,10 +7,10 @@ IMAGE_NAME="${DOCKER_USERNAME}/${REPO_NAME}"
 
 # Step 1: Build the image
 echo "Building Docker image..."
-sudo docker build -t ${IMAGE_NAME}:$COMMIT_ID -f build/dockerfile .
+docker build -t ${IMAGE_NAME}:$COMMIT_ID -f build/dockerfile .
 
 # Step 3: Push both tags to Docker Hub
 echo "Pushing dev Image"
-sudo docker push ${IMAGE_NAME}:$COMMIT_ID
+docker push ${IMAGE_NAME}:$COMMIT_ID
 
 echo "Done!"
